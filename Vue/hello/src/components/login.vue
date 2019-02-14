1da324bddbc0a9b85e46f9ce780c0f53870a349f
phamdinhthuytien <phamdinhthuytien@gmail.com>
2019-02-14 13:37:18 +0700

    Training vue

---
+++ Vue/hello/src/components/login.vue
@@ -0,0 +1,76 @@
+<template>
+  <form class="form-login">
+    <h2>LOGIN WITH FACEBOOK</h2>
+    <div class="wrap">
+      <span class="stroke"></span>
+      <p>or</p>
+      <span class="stroke"></span>
+    </div>
+    
+    <div>
+      <p>EMAIL ADDRESS</p>
+      <i class="fa fa-user" aria-hidden="true"></i>
+      <input placeholder="Email" type="text" name="email">
+    </div>
+    <div>
+      <p>PASSWORD</p>
+      <i class="fa fa-user" aria-hidden="true"></i>
+      <input placeholder="Password" type="password" name="password">
+    </div>
+    <div>LOGIN</div>
+    <div>
+      Create Account<spain>/</spain>Forgot Password
+    </div>
+  </form>
+</template>
+
+
+<script>
+export default {
+  name: 'loginComponent'
+};
+</script>
+
+<style scoped>
+
+.form-login {
+  width: 300px;
+  height: 400px;
+  background-color: #fff;
+  color: #000;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+h2 {
+  background-color: #3b5998;
+  font-size: 12px;
+  text-align: center;
+  font-weight: 600;
+  line-height: 50px;
+  border: none;
+  color: #fff;
+  letter-spacing: 2px;
+  margin: 0;
+}
+
+.stroke {
+  display: inline-block;
+  width: 100px;
+  height: 1px;
+  background-color: #b2b2b2;
+  margin: 0;
+  margin-top: 25px;
+}
+
+.wrap {
+  display: flex;
+  line-height: 50px;
+  text-align: center;
+}
+
+.wrap p {
+  margin: 0;
+}
+
+</style>
---
+++ Vue/hello/src/container/login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <p class="logo">meta.us</p>
+    <h1 class="title">Bring Players Together</h1>
+    <p class="content">
+      Sign to gain access to creating a Compertition Listing on meta.us. Customize A/B/C, Lorem ipsum dolar sit
+      amet consectebur. EU pretium justo rhoncus us desription of Competition feature.
+    </p>
+    <loginComponent />
+  </div>
+</template>
+
+
+<script>
+import loginComponent from '../components/login.vue'
+export default {
+  name: 'loginContainer',
+  components: {
+    loginComponent
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100vh;
+  background-image: url("../assets/background.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: #fff;
+}
+
+.logo {
+  font-size: 30px;
+  font-weight: 600;
+  margin: 0;
+  padding-top: 30px;
+}
+
+.title {
+  margin: 0;
+  font-size: 25px;
+  font-weight: 700;
+}
+
+.content {
+  max-width: 640px;
+  margin-left: auto;
+  margin-right: auto;
+  
+}
+
+</style>
